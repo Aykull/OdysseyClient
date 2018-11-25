82419237a1b05012c541e788c66ba3c7f50c9071
AyKull <luciamongeg@gmail.com>
2018-11-25 17:50:47 -0600

    Cliente con comunicacion Json

---
+++ cmake-build-debug/CMakeFiles/TECMFSClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TECMFSClient.dir/main.cpp.o"
+  "TECMFSClient.pdb"
+  "TECMFSClient"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TECMFSClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
